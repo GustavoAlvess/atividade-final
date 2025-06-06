2d42ff8947ad02162b8a886f903db13c13c37c54
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-06-06 08:43:31 -0300

    feat: adiciona exercicios como comentario

---
+++ scripts/atv-Inventarios.js
@@ -0,0 +1,147 @@
+let inventarioAlpha = [
+    ["Cristal Rubion", "Plutão", 4500],
+    ["Cápsula de Éter", "Júpiter", 7800],
+    ["Nanofibra Luminosa", "Saturno", 12000],
+    ["Minério Dourado", "Marte", 8500],
+    ["Flor de Andrômeda", "Andrômeda", 13000],
+    ["Ampulheta Galáctica", "Vênus", 2200],
+    ["Fóssil Estelar", "Lua", 9000],
+    ["Orbe do Caos", "Netuno", 9100],
+    ["Lente do Vazio", "Buraco Negro X-1", 14300],
+    ["Chave de Fóton", "Sol", 2300],
+    ["Esfera Sombria", "Nêmesis", 13400],
+    ["Granada de Íons", "Marte", 4100],
+    ["Tecido Estelar", "Urano", 5300],
+    ["Engrenagem Sombria", "Plutão", 6600],
+    ["Fragmento Prismático", "Kepler-22b", 6400],
+    ["Núcleo de Plasma", "Kepler-452b", 9100],
+    ["Anel de Chronos", "Terra", 4900],
+    ["Bateria Quântica", "Terra", 4000],
+    ["Areia do Tempo", "Terra", 3100],
+    ["Escudo de Luz", "Vênus", 4700],
+    ["Catalisador Estelar", "Sirius", 12500],
+    ["Pó de Cometa", "Netuno", 3000],
+    ["Relógio Quântico", "Terra", 5000],
+    ["Vórtice Gravitacional", "Saturno", 8800],
+    ["Cristal Ômega", "Andrômeda", 13900],
+    ["Cristal Ômega", "Andrômeda", 13900]
+  ];
+
+  let inventarioBeta = [
+    ["Lâmina Solar", "Mercúrio", 6700],
+    ["Cometa Engarrafado", "Netuno", 7600],
+    ["Relíquia de Zordon", "Elysium", 25000],
+    ["Pó Lunar", "Lua", 2900],
+    ["Nevoeiro Encapsulado", "Júpiter", 3800],
+    ["Chip de Matéria", "Ceres", 4100],
+    ["Chave de Portais", "Júpiter", 5900],
+    ["Globo de Plasma", "Terra", 7200],
+    ["Mapa Estelar", "Vênus", 2100],
+    ["Pedra Temporal", "Lua", 7800],
+    ["Lanterna Estelar", "Sol", 3200],
+    ["Fragmento de Eclipse", "Eclipse E-9", 8300],
+    ["Ponte de Gelo", "Hoth", 3900],
+    ["Fluido de Saturno", "Saturno", 4400],
+    ["Fenda de Tempo", "Marte", 8700],
+    ["Condutor Fotônico", "Sol", 3600],
+    ["Memória Galáctica", "Terra", 5500],
+    ["Livro de Nebulosa", "Andrômeda", 9200],
+    ["Túnel Quântico", "Netuno", 4100],
+    ["Canhão de Luz", "Vênus", 6800],
+    ["Orbe Incandescente", "Sol", 9300],
+    ["Matriz de Propulsão", "Vênus", 4500],
+    ["Partícula Livre", "Urano", 5400],
+    ["Capa de Invisibilidade", "Terra", 3900],
+    ["Terminal de Dobra", "Alpha Centauri", 10500]
+  ];
+  
+
+// Um item duplicado foi detectado no final do inventário Alpha. Remova a cópia.
+
+
+
+//Um artefato obsoleto foi removido do final da lista Beta após avaliação técnica.
+
+
+//Três novos artefatos da Terra foram entregues à nave Alpha. Adicione-os ao final.
+
+
+//Dois artefatos especiais de Kepler foram coletados e adicionados à nave Beta.
+
+
+//Um item da Alpha precisa aparecer no topo da lista para ser destacado na vitrine.
+
+
+//Um item da Beta deve ser promovido para o início da lista para estudo prioritário.
+
+
+//Um artefato incorreto foi inserido no topo da Alpha por engano. Ele deve ser excluído.
+
+
+//O primeiro item da Beta pertence a um museu e deve ser removido.
+
+
+//Verifique se a “Relíquia de Zordon” está presente no inventário Beta.
+
+
+//Verifique se o item “Nanofibra Luminosa” ainda está disponível na Alpha.
+
+
+//Gere uma string com todos os planetas do inventário Alpha, separados por um delimitador.
+
+
+//Crie um texto com os nomes dos 10 primeiros itens da Beta, separados por vírgulas.
+
+
+//Gere uma nova lista com os 5 primeiros itens da nave Beta para inspeção.
+
+
+//Separe os 10 últimos itens do inventário Alpha para estudo temporal.
+
+
+//O quinto item da Alpha foi substituído por uma versão mais moderna. Atualize.
+
+
+//Dois itens no meio do inventário Beta foram contaminados e precisam ser removidos.
+
+
+//Unifique os inventários Alpha e Beta em uma única estrutura.
+
+
+//Junte os cinco primeiros itens de cada nave em uma nova vitrine de destaques.
+
+
+//Crie uma versão com os nomes de todos os itens da Beta em letras maiúsculas.
+
+
+//Converta os valores de todos os itens da Alpha para uma moeda interestelar (multiplicação).
+
+
+//Filtre da Beta apenas os itens que custam mais de 7000 créditos.
+
+
+//Filtre da Alpha todos os itens originários da Terra para despacho diplomático.
+
+
+//Reorganize os itens da Beta do menor para o maior valor.
+
+
+//Reorganize a lista da Alpha em ordem alfabética dos nomes dos artefatos.
+
+
+//Inverta a ordem de todos os itens da Beta para visualização reversa.
+
+
+//Inverta a ordem dos 15 primeiros itens da Alpha como simulação de viagem no tempo.
+
+
+//Crie uma prévia com os nomes e planetas dos 10 itens mais valiosos da Alpha.
+
+
+//Extraia os itens da Beta que foram coletados em luas (ex: Lua, Europa, Titã…).
+
+
+//Identifique e mova para o final da Alpha todos os itens com valor abaixo de 4000.
+
+
+//Conte quantos itens do inventário Beta vieram de planetas do sistema solar interno.
